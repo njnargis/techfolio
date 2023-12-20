fa36bfb3fbd6c2e77051a2f3c69d7748bb06a4c6
njnargis <nargisjahan2020@gmail.com>
2023-12-20 22:26:18 +0530

    very first commit

---
+++ index1.js
@@ -0,0 +1,19 @@
+
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Designer", "Developer", "Tech Enthusiast", "Explorer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+   
+
+    
