9ebad299fc2cee5a392227dd154baca3e0b40587
Nargis Jahan <nargisjahan2020@gmail.com>
2024-09-20 16:32:20 +0530

    New Commit

--- index1.js
+++ index1.js
@@ -1,18 +1,18 @@
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Web Designer", "Developer", "Tech Enthusiast", "Explorer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
   
 
-   
 
-    
+   // toggle menu/navbar script
+   $('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Developer", "Tech Enthusiast", "Learner"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
